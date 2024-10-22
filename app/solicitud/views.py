0e1f42213035e2c98ed5b9d83bb99275976f7a5f
aleidaapaza <apazacolquealeidaninel@gmail.com>
2024-10-22 16:36:50 -0400

    view app

---
+++ app/solicitud/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.contrib.auth import logout
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, 
+
+class solicitud(TemplateView):
+    template_name = 'index.html'
