3020e799601cce66e71207aca2925fed63f60736
aleidaapaza <apazacolquealeidaninel@gmail.com>
2024-10-22 21:36:17 -0400

    solicitud1

--- app/proyecto/models.py
+++ app/proyecto/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
+from proyecto.choices import periodo_ejecucion
 # Create your models here.
+
+class DatosProyectoBase(models.Model):
+    nombre = models.CharField(max_length=255)
+    n_comunidades = models.IntegerField()
+    comunidades = models.TextField()
+    tipologia_proy = models.BooleanField()
+    periodo_ejecu = models.IntegerField(choices=periodo_ejecucion)
+
+    def __str__(self):
+        return f' {self.nombre}'
+    
+    class Meta:
+        verbose_name = _('DatosProyectoBase')
+        verbose_name_plural = _('DatosProyectosBase')
+        db_table = 'DatosProyectoBase'
--- app/solicitud/models.py
+++ app/solicitud/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
 from solicitud.choices import departamentos, entidad_territorial_autonoma, estado_proyecto, periodo_ejecucion
+from user.models import EncargadoMAE, ResponsableP
 # Create your models here.
 
 class Municipios (models.Model):
@@ -8,6 +10,7 @@ class Municipios (models.Model):
     entidad_territorial = models.CharField(choices=entidad_territorial_autonoma, max_length=100 )
     nombre_municipio = models.CharField(max_length=255)
     estado = models.CharField(choices=estado_proyecto, max_length=255)
+    p_a= models.BooleanField(default=False)
 
     def __str__(self):
         return f' {self.departamento}-{self.entidad_territorial}-{self.nombre_municipio}'
@@ -17,12 +20,16 @@ class Municipios (models.Model):
         verbose_name_plural = _('Municipios')
         db_table = 'Municipios'
 
-class DatosBase(models.Model):
-    nombre = models.CharField(max_length=255)
-    n_comunidades = models.IntegerField()
-    comunidades = models.TextField()
-    tipologia_proy = models.BooleanField()
-    periodo_ejecu = models.IntegerField(choices=periodo_ejecucion)
-
-class Postulaciones(models.Model):
+class Postulacion(models.Model):
     municipio = models.ForeignKey(Municipios, related_name='Datos_base_proyecto', on_delete=models.CASCADE)
+    mae = models.ForeignKey(EncargadoMAE, related_name='encargado_mae_p', on_delete=models.CASCADE)
+    responsable = models.ForeignKey(EncargadoMAE, related_name='responsable_p', on_delete=models.CASCADE)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f' {self.municipio.}'
+    
+    class Meta:
+        verbose_name = _('Postulacion')
+        verbose_name_plural = _('Postulaciones')
+        db_table = 'Postulaciones'
