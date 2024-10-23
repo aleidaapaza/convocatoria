d7c302b8174ac3f45a3fe627162e667916529fdd
aleidaapaza <apazacolquealeidaninel@gmail.com>
2024-10-23 11:56:50 -0400

    form_municipio

--- app/solicitud/urls.py
+++ app/solicitud/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 
-from solicitud.views import solicitud,entidad
+from solicitud.views import solicitud,entidad, municipio
 app_name = 'solicitud'
 urlpatterns = [
     path('', solicitud.as_view(), name='Departamento'),
-    path('solicitud/<str:departamento>/', entidad.as_view(), name='Entidad_Territorial'),
-    
+    path('solicitud/<int:departamento>/', entidad.as_view(), name='Entidad_Territorial'),
+    path('solicitud/<int:departamento>/<int:entidad>/', municipio.as_view(), name='Municipio'),
 ]
 
--- app/solicitud/views.py
+++ app/solicitud/views.py
@@ -4,13 +4,31 @@ from django.shortcuts import render
 
 from django.contrib.auth import logout
 from django.shortcuts import render
-from django.views.generic import TemplateView, View
+from django.views.generic import TemplateView
 from django.urls import reverse
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import CreateView, ListView
+from solicitud.models import Municipios
 
 class solicitud(TemplateView):
     template_name = 'index.html'
 
 class entidad(TemplateView):
     template_name = 'homepage/entidadTerritorial.html'
-    
+    def get_context_data(self, **kwargs):
+        context = super(entidad, self).get_context_data(**kwargs)
+        departamento = self.kwargs.get('departamento', 0)
+        context['dep'] = departamento
+        context['entity'] = 'ENTIDAD TERRITORIAL'
+        return context
+
+class municipio(ListView):
+    model = Municipios
+    template_name = 'homepage/municipio.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(entidad, self).get_context_data(**kwargs)
+        departamento = self.kwargs.get('departamento', 0)
+        
+        context['dep'] = departamento
+        context['entity'] = 'ENTIDAD TERRITORIAL'
+        return context
